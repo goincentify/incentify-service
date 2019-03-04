a42b844aa5a8a8ab8275d49a5d2157663692fd83
Luke Snell <ljsnell7@gmail.com>
2019-03-04 17:01:27 -0600

    Updated the endpoint to return a list of custom objects. Still need to load them into a db but it's a start.

--- src/main/java/com/incentify/incentifyapi/_controller/RewardController.java
+++ src/main/java/com/incentify/incentifyapi/_controller/RewardController.java
@@ -1,20 +1,15 @@
 package com.incentify.incentifyapi._controller;
 
+import java.io.IOException;
 import java.util.List;
 
-import com.fasterxml.jackson.databind.JsonNode;
-import com.incentify.incentifyapi._models.User;
-import com.incentify.incentifyapi._service.RewardLoader;
-import com.incentify.incentifyapi._service.UserRepository;
-import com.incentify.incentifyapi._service.UserService;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.incentify.incentifyapi._models.RewardItem;
+import com.incentify.incentifyapi._service.RewardLoader;
 
 @RestController
 class RewardController {
@@ -22,8 +17,8 @@ class RewardController {
 	// TEST
 	@ResponseBody
 	@RequestMapping(value = "/rewards/all", method = RequestMethod.GET, produces = "application/json")
-	JsonNode getRewards() {
-		RewardLoader rewardLoader = new RewardLoader();		
-		return rewardLoader.getRewards();	
+	List<RewardItem> getRewards() throws IOException {
+		RewardLoader rewardLoader = new RewardLoader();
+		return rewardLoader.createRewardsList(rewardLoader.getRewards());
 	}
 }
--- src/main/java/com/incentify/incentifyapi/_service/RewardLoader.java
+++ src/main/java/com/incentify/incentifyapi/_service/RewardLoader.java
@@ -1,24 +1,42 @@
 package com.incentify.incentifyapi._service;
 
 import java.io.File;
-import java.net.URL;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.incentify.incentifyapi._models.RewardItem;
 
 @Service
 public class RewardLoader {
 //	@Autowired
 //	ReadJson readJson;
 
-	public JsonNode getRewards() {
+	public String getRewards() throws IOException {
 		ReadJson readJson = new ReadJson();
-		   File directory = new File("./src/main/java/com/incentify/incentifyapi/_models/_defaultRewards.json");
-		   System.out.println(directory.getAbsolutePath());
+		File directory = new File("./src/main/java/com/incentify/incentifyapi/_models/_defaultRewards.json");
 		return readJson.readFileAsString(directory.getAbsolutePath());
-
 	}
 
+	public List<RewardItem> createRewardsList(String jsonRewardList) throws IOException {
+		List<RewardItem> rewardItems = new ArrayList<RewardItem>();
+		ObjectMapper jsonMapper = new ObjectMapper();
+		try {
+			System.out.println(jsonRewardList);
+			System.out.println(
+					"JsonMapper: " + jsonMapper.readValue(jsonRewardList, new TypeReference<List<RewardItem>>() {
+					}));
+			rewardItems = jsonMapper.readValue(jsonRewardList, new TypeReference<List<RewardItem>>() {
+			});
+		} catch (JsonProcessingException e) {
+			// Definitely a hand written auto catch block.
+			e.printStackTrace();
+		}
+		return rewardItems;
+	}
 }
